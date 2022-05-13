72abcb224e26da5acf959b66e328b5630b373617
ioannis_rom <ioannisromanelis@gmail.com>
2022-05-13 21:02:05 +0300

    add two_crops_rotated

--- datasets/ready_datasets.py
+++ datasets/ready_datasets.py
@@ -13,7 +13,7 @@ def get_ModelNet40(path, name="original", batch_size=32, drop_last=False):
     #
     # Returns:  the train and validation dataloader
 
-    assert name in ["original", "rotated", "two_crops_original"]
+    assert name in ["original", "rotated", "two_crops_original", "two_crops_rotated"]
 
     if name == "original":
         train_dataset = ModelNet40Sampled(path, num_points=1024, partition='train')
@@ -48,6 +48,25 @@ def get_ModelNet40(path, name="original", batch_size=32, drop_last=False):
         train_dataset = ModelNet40SampledCustom(path, num_points=1024, partition='train', transforms=train_transforms)
         valid_dataset = ModelNet40Sampled(path, num_points=1024, partition='test')
 
+    elif name == "two_crops_rotated":
+
+        train_transforms = TwoCropsTransform([
+                                RandomPointDropout(), 
+                                RandomShuffle(), 
+                                AnisotropicScale(), 
+                                ToTensor(),
+                                RandomRotate(180, 0),
+                                RandomRotate(180, 1),
+                                RandomRotate(180, 2)])    
+
+        valid_transforms = [RandomShuffle(), 
+                            ToTensor(),
+                            RandomRotate(180, 0),
+                            RandomRotate(180, 1),
+                            RandomRotate(180, 2)]
+        
+        train_dataset = ModelNet40SampledCustom(path, num_points=1024, partition='train', transforms=train_transforms)
+        valid_dataset = ModelNet40SampledCustom(path, num_points=1024, partition='test' , transforms=valid_transforms)
 
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=8, drop_last=drop_last)
     valid_loader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=False, num_workers=8, drop_last=False)
